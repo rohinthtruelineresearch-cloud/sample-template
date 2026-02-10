b9f908a424fbbd689bc95e638706320474390ba6
rohinthtruelineresearch-cloud <rohinthtruelineresearch@gmail.com>
2026-02-10 14:35:46 +0530

    Fix Render Module Not Found: create root proxy server.js

---
+++ server.js
@@ -0,0 +1,5 @@
+// 🚀 Render Proxy Server
+// This file exists to satisfy Render's default "node server.js" command
+// It redirects to the actual server code in the /server directory
+
+require('./server/server.js');
